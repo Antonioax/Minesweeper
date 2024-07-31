6b346d69f69a427f48856358787b640339bf67e5
Antonio Jurić <juricantonio18@gmail.com>
2024-07-31 21:44:59 +0200

    Added numbers visualization

--- app.js
+++ app.js
@@ -21,59 +21,76 @@ document.addEventListener("DOMContentLoaded", () => {
 
       grid.appendChild(square);
       squares.push(square);
+
+      square.addEventListener("click", function (e) {
+        click(square);
+      });
     }
-  }
 
-  for (let i = 0; i < squares.length; ++i) {
-    let total = 0;
+    for (let i = 0; i < squares.length; ++i) {
+      let total = 0;
 
-    const isLeftEdge = i % width === 0;
-    const isRightEdge = i % width === width - 1;
+      const isLeftEdge = i % width === 0;
+      const isRightEdge = i % width === width - 1;
 
-    if (squares[i].classList.contains("empty")) {
-      if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb"))
-        total++;
-      if (
-        i > width - 1 &&
-        !isRightEdge &&
-        squares[i + 1 - width].classList.contains("bomb")
-      )
-        total++;
-      if (i > width && squares[i - width].classList.contains("bomb")) total++;
-      if (
-        i > width + 1 &&
-        !isLeftEdge &&
-        squares[i - 1 - width].classList.contains("bomb")
-      )
-        total++;
+      if (squares[i].classList.contains("empty")) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb"))
+          total++;
+        if (
+          i > width - 1 &&
+          !isRightEdge &&
+          squares[i + 1 - width].classList.contains("bomb")
+        )
+          total++;
+        if (i > width && squares[i - width].classList.contains("bomb")) total++;
+        if (
+          i > width + 1 &&
+          !isLeftEdge &&
+          squares[i - 1 - width].classList.contains("bomb")
+        )
+          total++;
 
-      if (
-        i < width * width - 2 &&
-        !isRightEdge &&
-        squares[i + 1].classList.contains("bomb")
-      )
-        total++;
-      if (
-        i < width * width - width &&
-        !isLeftEdge &&
-        squares[i - 1 + width].classList.contains("bomb")
-      )
-        total++;
-      if (
-        i < width * width - width - 2 &&
-        !isRightEdge &&
-        squares[i + 1 + width].classList.contains("bomb")
-      )
-        total++;
-      if (
-        i < width * width - width - 1 &&
-        squares[i + width].classList.contains("bomb")
-      )
-        total++;
+        if (
+          i < width * width - 2 &&
+          !isRightEdge &&
+          squares[i + 1].classList.contains("bomb")
+        )
+          total++;
+        if (
+          i < width * width - width &&
+          !isLeftEdge &&
+          squares[i - 1 + width].classList.contains("bomb")
+        )
+          total++;
+        if (
+          i < width * width - width - 2 &&
+          !isRightEdge &&
+          squares[i + 1 + width].classList.contains("bomb")
+        )
+          total++;
+        if (
+          i < width * width - width - 1 &&
+          squares[i + width].classList.contains("bomb")
+        )
+          total++;
 
-      squares[i].setAttribute("data", total);
+        squares[i].setAttribute("data", total);
+      }
     }
   }
 
   createBoard();
+
+  function click(square) {
+    if (square.classList.contains("bomb")) {
+      alert("Game Over!");
+    } else {
+      let total = square.getAttribute("data");
+      if (total != 0) {
+        square.classList.add("checked");
+        square.innerHTML = total;
+        return;
+      }
+    }
+  }
 });
