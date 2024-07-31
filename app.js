8fbd59dc1f33a84484ac3daf7e6cb6cbefcdfb77
Antonio Jurić <juricantonio18@gmail.com>
2024-07-31 21:08:31 +0200

    Added random shuffle

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
 
   let width = 10;
+  let bombAmount = 20;
   let squares = [];
 
   function createBoard() {
+    const allBombs = Array(bombAmount).fill("bomb");
+    const allEmpty = Array(width * width - bombAmount).fill("empty");
+
+    const allBoxes = allEmpty.concat(allBombs);
+
+    const allShuffled = allBoxes.sort(() => Math.random() - 0.5);
+
     for (let i = 0; i < width * width; ++i) {
       const square = document.createElement("div");
       square.setAttribute("id", i);
