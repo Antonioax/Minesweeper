08451edad9513a927228e7c13f68710dd9fa55ec
Antonio Jurić <juricantonio18@gmail.com>
2024-07-31 21:11:04 +0200

    Added initial visualization

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (let i = 0; i < width * width; ++i) {
       const square = document.createElement("div");
+      
       square.setAttribute("id", i);
+      square.classList.add(allShuffled[i]);
 
       grid.appendChild(square);
       squares.push(square);
